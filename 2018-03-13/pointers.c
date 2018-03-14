db2c4203721ee56bbbfe5ce90a870391c9ea43ac
Edgard Arvelaez <ealejandro@outlook.com>
2018-03-13 21:35:51 -0400

    Fixed the pointers, sorting examples

--- 2018-03-13/pointers.c
+++ 2018-03-13/pointers.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-struct student 
-{
-    char name[21];
-    int age;
-};
-
 int main(void)
 {
     int a = 0;
     int* b = &a;
+
     printf("a = %d, &a = %p\n", a, &a);
     printf("b = %p, *b = %d\n", b, *b);
     return 0;
--- 2018-03-13/sort.c
+++ 2018-03-13/sort.c
@@ -2,11 +2,33 @@
 
 int sort(int* arrs, int size)
 {
+    for(int i = 0; i < size; ++i) {
+        for(int j = 0; j < size; ++j) {
+            if(arrs[i] < arrs[j]) {
+                int tmp = arrs[i];
+                arrs[i] = arrs[j];
+                arrs[j] = tmp;
+            }
+        }
+    }
 }
 
 int main(void)
 {
     int nums[] = { 6, 7, 4, 2, 3, 1, 9, 8, 0 };
-    sort(nums, 10);
+    int size = 10;
+
+    printf("Not sorted: ");
+    for(int i = 0; i < size; ++i) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+    sort(nums, size);
+
+    printf("Sorted: ");
+    for(int i = 0; i < size; ++i) {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
     return 0;
 }
